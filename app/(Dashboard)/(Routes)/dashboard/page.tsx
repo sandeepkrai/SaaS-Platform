d32da2fc99d6eab4e3484270a39542848a215301
sandeepkrai <raisandeep.dev@gmail.com>
2023-12-20 19:49:03 +0530

    SaaS Platform

---
+++ app/(Dashboard)/(Routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+// @flow 
+import { Button } from '@/components/ui/button';
+import * as React from 'react';
+
+const DashboardPage = () => {
+    return <Button>Click Me!</Button>;
+};
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const LandingPage = () => {
+  return <div>Landing Page (Unprotected)</div>;
+};
+
+export default LandingPage;
